4253183440a65c4c4ca6a6b6815f76bfc4dec106
Joel <tanunajoel0@gmail.com>
2023-11-08 09:36:10 +0100

    reply comments

--- src/Constants.js
+++ src/Constants.js
@@ -1,101 +1,101 @@
-export const posts = [
-    {
-        id: 1,
-        time: 1,
-        username: "whiteman",
-        firstName: "",
-        lastName: "",
-        email: "",
-        post: "lorem. sdihi isduhiud iohv dsvoi sddsdo isdio isdouids sudihvurei e uieru v",
-        profilePic: require("../assets/images/1.jpeg"),
-         content: {
-      type: "image", // "image" for images, "video" for videos
-      source: require("../assets/images/4.jpeg"),
-    },
-        likes: 200,
-        comments: 500,
-        saved: 20,
-        shared: 150,
-        followers: 200,
-        following: 900,
-    },
-    {
-        id: 2,
-        time: 2,
-        username: "creed",
-        firstName: "",
-        lastName: "",
-        email: "",
-        post: "lorem. sdihi ojoje wa;c;j reijbda, abdhj ,ahbuuw jsv sjdh sdd isduhiudv",
-        profilePic: require("../assets/images/2.jpeg"),
-         content: {
-      type: "video", // "image" for images, "video" for videos
-      source: require('../assets/images/fam.mp4'),
-    },
-        likes: 800,
-        comments: 500,
-        saved: 20,
-        shared: 150,
-        followers: 200,
-        following: 900,
-    },
-    {
-        id: 3,
-        time: 3,
-        username: "machine",
-        firstName: "",
-        lastName: "",
-        email: "",
-        post: "lorem. sdihi isduh fpe e0-0w w-0ev fof ioe oefiv iudv",
-        profilePic: require("../assets/images/3.jpg"),
-         content: {
-      type: "image", // "image" for images, "video" for videos
-      source: require("../assets/images/1.jpeg"),
-    },
-        likes: 1000,
-        comments: 50,
-        saved: 120,
-        shared: 10,
-        followers: 200,
-        following: 900,
-    },
-    {
-        id: 4,
-        time: 4,
-        username: "Sani",
-        firstName: "",
-        lastName: "",
-        email: "",
-        post: "lorem. sdihi isdvbp fpodf dpfiej reio ero e uhiudv",
-        profilePic: require("../assets/images/4.jpeg"),
-         content: {
-      type: "video", // "image" for images, "video" for videos
-      source: require('../assets/images/hum.mp4'),
-    },
-    likes: 20,
-        comments: 5,
-        saved: 920,
-        shared: 9150,
-        followers: 200,
-        following: 900,
-    },
-];
+// export const posts = [
+//     {
+//         id: 1,
+//         time: 1,
+//         username: "whiteman",
+//         firstName: "",
+//         lastName: "",
+//         email: "",
+//         post: "lorem. sdihi isduhiud iohv dsvoi sddsdo isdio isdouids sudihvurei e uieru v",
+//         profilePic: require("../assets/images/1.jpeg"),
+//          content: {
+//       type: "image", // "image" for images, "video" for videos
+//       source: require("../assets/images/4.jpeg"),
+//     },
+//         likes: 200,
+//         comments: 500,
+//         saved: 20,
+//         shared: 150,
+//         followers: 200,
+//         following: 900,
+//     },
+//     {
+//         id: 2,
+//         time: 2,
+//         username: "creed",
+//         firstName: "",
+//         lastName: "",
+//         email: "",
+//         post: "lorem. sdihi ojoje wa;c;j reijbda, abdhj ,ahbuuw jsv sjdh sdd isduhiudv",
+//         profilePic: require("../assets/images/2.jpeg"),
+//          content: {
+//       type: "video", // "image" for images, "video" for videos
+//       source: require('../assets/images/fam.mp4'),
+//     },
+//         likes: 800,
+//         comments: 500,
+//         saved: 20,
+//         shared: 150,
+//         followers: 200,
+//         following: 900,
+//     },
+//     {
+//         id: 3,
+//         time: 3,
+//         username: "machine",
+//         firstName: "",
+//         lastName: "",
+//         email: "",
+//         post: "lorem. sdihi isduh fpe e0-0w w-0ev fof ioe oefiv iudv",
+//         profilePic: require("../assets/images/3.jpg"),
+//          content: {
+//       type: "image", // "image" for images, "video" for videos
+//       source: require("../assets/images/1.jpeg"),
+//     },
+//         likes: 1000,
+//         comments: 50,
+//         saved: 120,
+//         shared: 10,
+//         followers: 200,
+//         following: 900,
+//     },
+//     {
+//         id: 4,
+//         time: 4,
+//         username: "Sani",
+//         firstName: "",
+//         lastName: "",
+//         email: "",
+//         post: "lorem. sdihi isdvbp fpodf dpfiej reio ero e uhiudv",
+//         profilePic: require("../assets/images/4.jpeg"),
+//          content: {
+//       type: "video", // "image" for images, "video" for videos
+//       source: require('../assets/images/hum.mp4'),
+//     },
+//     likes: 20,
+//         comments: 5,
+//         saved: 920,
+//         shared: 9150,
+//         followers: 200,
+//         following: 900,
+//     },
+// ];
 
-// staticMessages.js
+// // staticMessages.js
 
-export const staticMessages = [
-    {
-        sender: 'user1',
-        text: 'CachoF Game previews and reviews, games are universal, therefore you need to win the game. wao soo interesting keep it up',
-        timestamp: new Date(),
-        source: require('../assets/images/hum.mp4'),
-    },
-    {
-        sender: 'user2',
-        text: 'CachoF Game previews and reviews, games are universal, therefore you need to win the game. wao soo interesting keep it up',
-        timestamp: new Date(),
-        source: require('../assets/images/hum.mp4'),
-    },
-    // Add more messages as needed
-  ];
+// export const staticMessages = [
+//     {
+//         sender: 'user1',
+//         text: 'CachoF Game previews and reviews, games are universal, therefore you need to win the game. wao soo interesting keep it up',
+//         timestamp: new Date(),
+//         source: require('../assets/images/hum.mp4'),
+//     },
+//     {
+//         sender: 'user2',
+//         text: 'CachoF Game previews and reviews, games are universal, therefore you need to win the game. wao soo interesting keep it up',
+//         timestamp: new Date(),
+//         source: require('../assets/images/hum.mp4'),
+//     },
+//     // Add more messages as needed
+//   ];
   
